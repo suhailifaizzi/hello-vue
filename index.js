4462ab2d6d1bbab6e8bd8db682a92f544b2713d4
Suhaili Faizzi <suhailifaizzi@Suhailis-MacBook-Pro.local>
2019-07-17 10:44:56 +0800

    Added conditional and ui handling exercise

--- index.js
+++ index.js
@@ -5,4 +5,48 @@ var app = new Vue({
   }
 })
 
+var app2  = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
 app.message = 'I have changes the data';
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    //seen: true    // Make object visible
+    seen: false   // Hide object
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn Javascript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!',  
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
